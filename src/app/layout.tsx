a635cf3fb43e660d7e05cc818d1e9e1ea5931b04
Stephen Amoako <stevekobbi20@gmail.com>
2025-09-29 18:17:21 +0000

    i still see the icon of my website as a rea ct js icon how do i change i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable, fontMono.variable)}>
         <AppProviders>
           {children}
